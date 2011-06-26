46206bb4fd8d83b9aa5ba7db9398370c1e36db20
jjnguy <justin.john.nelson@gmail.com>
2011-06-26 01:23:56 +0000

    Fixed ExtenededNodeList item(int) method.  MAde to comply with the documentation.

--- src/xmlcomponents/complex/ExtendedNodeList.java
+++ src/xmlcomponents/complex/ExtendedNodeList.java
@@ -12,77 +12,79 @@ import xmlcomponents.JodeFilter;
 
 public class ExtendedNodeList implements NodeList, Iterable<ExtendedNode> {
 
-	private List<ExtendedNode> jodes;
+   private List<ExtendedNode> jodes;
 
-	public ExtendedNodeList(NodeList list) {
-		this(list, false);
-	}
+   public ExtendedNodeList(NodeList list) {
+      this(list, false);
+   }
 
-	public ExtendedNodeList(NodeList list, boolean retainWhitespace) {
-		jodes = new ArrayList<ExtendedNode>(list.getLength());
-		for (int i = 0; i < list.getLength(); i++) {
-			ExtendedNode toAdd = new ExtendedNode(list.item(i));
-			if (!toAdd.isWhiteSpace() || retainWhitespace) {
-				jodes.add(toAdd);
-			}
-		}
-	}
+   public ExtendedNodeList(NodeList list, boolean retainWhitespace) {
+      jodes = new ArrayList<ExtendedNode>(list.getLength());
+      for (int i = 0; i < list.getLength(); i++) {
+         ExtendedNode toAdd = new ExtendedNode(list.item(i));
+         if (!toAdd.isWhiteSpace() || retainWhitespace) {
+            jodes.add(toAdd);
+         }
+      }
+   }
 
-	private ExtendedNodeList(List<ExtendedNode> jodes) {
-		this.jodes = jodes;
-	}
+   private ExtendedNodeList(List<ExtendedNode> jodes) {
+      this.jodes = jodes;
+   }
 
-	public ExtendedNode first() {
-		if (getLength() == 0)
-			throw new JinqException("There is no first node");
-		return item(0);
-	}
+   public ExtendedNode first() {
+      if (getLength() == 0)
+         throw new JinqException("There is no first node");
+      return item(0);
+   }
 
-	public ExtendedNode single(String nodeName) {
-		ExtendedNodeList lst = this.filter(nodeName);
-		if (lst.getLength() != 1)
-			throw new JinqException("The call to 'single' did not return 1 result.");
-		return lst.item(0);
-	}
+   public ExtendedNode single(String nodeName) {
+      ExtendedNodeList lst = this.filter(nodeName);
+      if (lst.getLength() != 1)
+         throw new JinqException("The call to 'single' did not return 1 result.");
+      return lst.item(0);
+   }
 
-	public ExtendedNode single(JodeFilter filter) {
-		ExtendedNodeList lst = this.filter(filter);
-		if (lst.getLength() != 1)
-			throw new JinqException("The call to 'single' did not return 1 result.");
-		return lst.item(0);
-	}
+   public ExtendedNode single(JodeFilter filter) {
+      ExtendedNodeList lst = this.filter(filter);
+      if (lst.getLength() != 1)
+         throw new JinqException("The call to 'single' did not return 1 result.");
+      return lst.item(0);
+   }
 
-	public ExtendedNodeList filter(final String nodeName) {
-		return this.filter(new JodeFilter() {
-			@Override
-			public boolean accept(Jode j) {
-				return j.extend().getNodeName().equals(nodeName);
-			}
-		});
-	}
+   public ExtendedNodeList filter(final String nodeName) {
+      return this.filter(new JodeFilter() {
+         @Override
+         public boolean accept(Jode j) {
+            return j.extend().getNodeName().equals(nodeName);
+         }
+      });
+   }
 
-	public ExtendedNodeList filter(JodeFilter filter) {
-		List<ExtendedNode> result = new ArrayList<ExtendedNode>();
-		for (ExtendedNode j : this) {
-			if (filter.accept(new Jode(j))) {
-				result.add(j);
-			}
-		}
-		return new ExtendedNodeList(result);
-	}
+   public ExtendedNodeList filter(JodeFilter filter) {
+      List<ExtendedNode> result = new ArrayList<ExtendedNode>();
+      for (ExtendedNode j : this) {
+         if (filter.accept(new Jode(j))) {
+            result.add(j);
+         }
+      }
+      return new ExtendedNodeList(result);
+   }
 
-	@Override
-	public int getLength() {
-		return jodes.size();
-	}
+   @Override
+   public int getLength() {
+      return jodes.size();
+   }
 
-	@Override
-	public ExtendedNode item(int arg0) {
-		return jodes.get(arg0);
-	}
+   @Override
+   public ExtendedNode item(int arg0) {
+      if (arg0 >= getLength() || arg0 < 0)
+         return null;
+      return jodes.get(arg0);
+   }
 
-	@Override
-	public Iterator<ExtendedNode> iterator() {
-		return jodes.iterator();
-	}
+   @Override
+   public Iterator<ExtendedNode> iterator() {
+      return jodes.iterator();
+   }
 }
